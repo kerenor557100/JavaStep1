6faa2858835c18bb0202aae91236606590138d8d
keren_or <57459738+kerenor557100@users.noreply.github.com>
2020-03-19 21:29:56 +0200

    Update Plane.java

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -1,15 +1,38 @@
 package geometries;
 
-import primitives.Coordinate;
 import primitives.Point3D;
 import primitives.Vector;
 
-public class Plane {
-    public Plane(Point3D vertex, Point3D vertex1, Point3D vertex2) {
+public class Plane implements Geometry {
+
+    Point3D _p;
+    primitives.Vector _normal;
+
+    public Plane(Point3D p1, Point3D p2, Point3D p3) {
+        _p = new Point3D(p1);
+
+        Vector U = new Vector(p1, p2);
+        Vector V = new Vector(p1, p3);
+        Vector N = U.crossProduct(V);
+        N.normalize();
+
+        _normal = N.scale(-1);
+
     }
 
-    public Vector getNormal() {
-        return Vector.ZERO;
+    public Plane(Point3D _p, Vector _normal) {
+        this._p = new Point3D(_p);
+        this._normal = new Vector(_normal);
     }
-    //TO DO
+
+    @Override
+    public Vector getNormal(Point3D p) {
+        return _normal;
+    }
+
+    //because polygon
+    public Vector getNormal() {
+        return getNormal(null);
+    }
+
 }
